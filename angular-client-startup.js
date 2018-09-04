c5ebe470109c86fc7ec340343e48d936a5d2659e
Yogesh Sonawane <yogesh-sonawane@users.noreply.github.com>
2018-09-04 20:35:37 +0530

    'changed'

--- angular-client-startup.js
+++ angular-client-startup.js
@@ -3,8 +3,9 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
 
+/*
 const forceSsl = function () {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -15,16 +16,16 @@ const forceSsl = function () {
     next();
   }
 }
-
 app.use(forceSsl());
+*/
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 var portNumber = process.env.PORT || 57012;
 app.listen(portNumber, function () {
   console.log('\n=======================================================================\n');
-  console.log(`Yogeshs-angular-client-app is up running on port: ${portNumber}\n`);
+  console.log(`yogeshs-angular-client-app is up running on port: ${portNumber}\n`);
   console.log('=======================================================================\n');
 });
