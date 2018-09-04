a80c7ed95f95d08a1bc429d1e21ac6bb58cd8459
Yogesh Sonawane <yogesh-sonawane@users.noreply.github.com>
2018-09-04 19:31:49 +0530

    changed package.json

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,7 +1,9 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+const {
+  SpecReporter
+} = require('jasmine-spec-reporter');
 const portNumber = process.env.PORT || 3000;
 exports.config = {
   allScriptsTimeout: 11000,
@@ -12,17 +14,21 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'https://powerful-shore-30887.herokuapp.com:57012/',
+  baseUrl: 'https://powerful-shore-30887.herokuapp.com:' + portNumber + "/",
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () {}
   },
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(new SpecReporter({
+      spec: {
+        displayStacktrace: true
+      }
+    }));
   }
 };
